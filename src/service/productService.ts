1b4800f1190da91a5ba8e69d98800f8f42057435
phong0710 <phonglh071099@gmail.com>
2023-08-02 23:32:15 +0700

    xong productService

---
+++ src/service/productService.ts
@@ -0,0 +1,58 @@
+import { Category } from './../entity/categogy';
+import { AppDataSource } from "src/data-source";
+import { Product } from "src/entity/product";
+
+class ProductService{
+    private productRepository;
+    constructor(){
+        this.productRepository = AppDataSource.getRepository(Product)
+    }
+
+    getAll = async (page, pageSize, getTotalCount=false)=>{
+        const skip = (page -1)* pageSize;
+        const take = pageSize;
+
+        const [products,total]= await this.productRepository.findAndCount({
+            relations:{
+                category:true
+            },
+            skip,
+            take,
+        });
+        const totalPages = Math.ceil(total/pageSize);
+        if(getTotalCount){
+            return {
+                products,
+                totalCount: total,
+                totalPages,
+            }
+        } else {
+            return{
+                products,
+                totalPages
+            }
+        }
+    }
+
+    add = async (product)=>{
+        await this.productRepository.save(product);
+    }
+
+    remove = async (id)=>{
+        let products = await this.productRepository.find(
+           {
+            where:{
+                product:{idProduct:id}
+            }
+           }
+        )
+        if(products){
+            await this.productRepository.delete({
+                where:{
+                    products:{idProduct:id}
+                }
+            })
+        }
+    }
+}
+export default new ProductService();
