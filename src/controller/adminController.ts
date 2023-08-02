5fdd00d0d3f1894d1a35aea4045e8d31f7f9e097
duylo <duyvu06497@gmail.com>
2023-08-02 20:34:58 +0700

    lam xong entity admin,user,service login,controller

---
+++ src/controller/adminController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import adminServive from "src/service/adminServive";
+class AdminController {
+    checkLogin = async (req: Request, res: Response) => {
+        try {
+            let admin = await adminServive.loginCheck(req.body);
+
+            if (admin) {
+                res.status(200).json({
+                    success: true,
+                    data: admin.info,
+                });
+            } else {
+                throw new Error("Wrong email or password");
+            }
+        } catch (e) {
+            console.log(" error in login ", e);
+            res.status(500).json({
+                message: "error in login",
+                success: false,
+            });
+        }
+    };
+}
---
+++ src/controller/userController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { decode } from "punycode";
+import * as bcrypt from "bcrypt";
+import userService from "src/service/userService";
+class UserController {
+    findBillByPhoneNumber = async (req: Request, res: Response) => {
+        try {
+            let bills =await userService.findBillByPhoneNumber(req.body.phoneNumber);
+            res.status(200).json({
+                success: true,
+                data: bills,
+            });
+        } catch (e) {
+            console.log(" error in findBillByPhoneNumber", e);
+            res.status(500).json({
+                message: "error you are anonymous",
+                success: false,
+            });
+        }
+    };
+}
