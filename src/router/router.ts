8433f30adf6ead8b4be753e17f80052563c7334f
Your Name <you@example.com>
2023-08-03 09:04:09 +0700

    da merge nhanh Feature/product(bi xung dot file router)

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,5 +3,5 @@ import productRouter from "./productRouter";
 import categoryRouter from "./categoryRouter";
 
 const router = Router();
-router.use('product',productRouter)
+router.use('product',productRouter);
 router.use('/category', categoryRouter);
